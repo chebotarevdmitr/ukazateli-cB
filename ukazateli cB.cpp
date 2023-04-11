ddad6f3e3255cd6848a2ca8a3c0cf2b2fbc656c4
DIM <4ebotarov_dim@inbox.ru>
2023-04-11 23:51:36 +0300

    add min

--- ukazateli cB.cpp
+++ ukazateli cB.cpp
@@ -1,11 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
 
-int()
+int getMin(int numbers[], int size) {
+    int min = numbers[0];
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+}
 
 int main()
 {
     int numbers[5] = { 5,4,-2,29,6 };
+    cout << "Min is =" << getMin(numbers, 5);
 
     system("pause>0");
     return 0;
