e0473b8c92c59708253d830fe5f521647138ef17
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-12 00:20:44 +0300

    max

--- ukazateli cB.cpp
+++ ukazateli cB.cpp
@@ -24,7 +24,7 @@ int main()
 {
     int numbers[5] = { 5,4,-2,29,6 };
     cout << "Min is =" << getMin(numbers, 5)<<endl;
-    cout << "Min is =" << getMin(numbers, 5)<<endl;
+    cout << "Max is =" << getMax(numbers, 5)<<endl;
     system("pause>0");
     return 0;
 }
